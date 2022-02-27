9ba0055c94632496ef7bc9d102239025004b08b0
Ankomat <ankomat@live.be>
2022-02-27 13:57:20 +0100

    testing structs

---
+++ src/bin/structs.rs
@@ -0,0 +1,31 @@
+#[derive(Debug)]
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email,
+        username,
+        active: true,
+        sign_in_count: 1,
+    }
+}
+
+fn main() {
+    let user1 = build_user(String::from("me@example.com"), String::from("me"));
+
+    // let user2 = User {
+    //     active: user1.active,
+    //     username: user1.username,
+    //     email: String::from("another@example.com"),
+    //     sign_in_count: user1.sign_in_count,
+    // };
+
+    let user2 = build_user(user1.email.clone(), user1.username.clone());
+
+    println!("{:?}", user1);
+}
